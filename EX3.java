6959496d22ef9a27f6551143de76e88958e6909c
USMAN <93647959+usman1009@users.noreply.github.com>
2022-03-22 14:25:23 +0530

    Add files via upload

---
+++ EX3.java
@@ -0,0 +1,25 @@
+class product {
+	int pcode,price;
+	String pname;
+	void getdata(int x,String y,int z) {
+	pcode=x;
+	pname=y;
+	price=z;
+	}
+}
+
+public class EX3{
+	public static void main(String args[]) {
+	 product p1 = new product();
+	 product p2 = new product();
+	 product p3 = new product();
+	 p1.getdata(123,"LAYS",10);
+	 p2.getdata(234,"BANANA",40);
+	 p1.getdata(456,"NOODLES",12);
+	 System.out.println("maximun price is:"+Math.max(p1.price,Math.max(p2.price,p3.price)));
+	 }
+}
+
+
+						
+
